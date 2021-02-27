10233993ef66aee09741c8674ba748733d257d9e
Mikael Holmgren <mikael.holmgren@iths.se>
2021-02-27 10:54:54 +0100

    Add project files.

---
+++ Geolocation/Pages/Index.cshtml.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Geolocation.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+        public float? Long { get; set; }
+    public float? Lat { get; set; }
+    public void OnGet(float? longitude, float? latitude)
+        {
+            Long = longitude;
+            Lat = latitude;
+        }
+    }
+}
